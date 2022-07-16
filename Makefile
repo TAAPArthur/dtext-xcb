d262d10e38a18759c5f3f38677dca6f4745a4c31
Arthur Williams <taaparthur@disroot.org>
2022-07-15 23:44:17 -0700

    Convert into a single-header library
    Update license and README to differentiate dtext-xcb from the original
    dtext. Also convert this into a single header library.
    
    Now only a single header dtext_xcb.h is installed

--- Makefile
+++ Makefile
@@ -1,38 +1,9 @@
-FT_CFLAGS  = -I /usr/include/freetype2
-FT_LDFLAGS = -lfreetype
+all:
 
-XLIB_LDFLAGS = -lxcb -lxcb-render -lxcb-render-util
+install:
+	install -Dt $(DESTDIR)/usr/include/ dtext_xcb.h
 
-CFLAGS  ?= -std=c99 -Wall
-CFLAGS  += ${FT_CFLAGS}
-LDFLAGS += ${XLIB_LDFLAGS} ${FT_LDFLAGS}
+uninstall:
+	rm $(DESTDIR)/usr/include/dtext_xcb.h
 
-ifdef DEBUG
-CFLAGS += -g
-endif
-
-SRCS := $(wildcard *.c)
-EXECS := $(patsubst examples/%.c,build/%,$(wildcard examples/*.c))
-
-all: libdtext.so
-
-libdtext.so: $(wildcard *.c)
-	${CC} ${CFLAGS} -fPIC -shared -o $@ $^ ${LDFLAGS}
-
-install: libdtext.so
-	install -Dt $(DESTDIR)/usr/lib/ $^
-	install -Dt $(DESTDIR)/usr/include/dtext/ *.h
-
-examples: $(EXECS)
-
-build:
-	mkdir build
-
-
-build/%: examples/%.c $(SRCS)  | build
-	${CC} -I. ${CFLAGS} -g ${LDFLAGS} $^ -o $@
-
-clean:
-	rm -f $(EXECS) *.so
-
-.PHONY: all clean
+.PHONY: all
--- dtext_xcb.h
+++ dtext_xcb.h
@@ -1,18 +1,99 @@
 /* See LICENSE file for copyright and license details. */
+/**
+ * To define the implemenation, define  DTEXT_XCB_IMPLEMENTATION before including this file
+ *
+ * Also you may need to add
+ * `-I /usr/include/freetype2` to CFLAGS And  `-lfreetype -lxcb -lxcb-render -lxcb-render-util` to LDFLAGS
+ * to get this program to compile
+ */
+#ifndef DTEXT_H
+#define DTEXT_H
+
+#include <stdint.h>
+#include <xcb/xcb.h>
 
 
+typedef struct dt_context dt_context;
+typedef struct dt_font dt_font;
+
+/**
+ * returns the height of the highest character of the font, relative to the baseline
+ */
+uint16_t dt_get_font_ascent(dt_font* font);
+/**
+ * returns the total height of the highest character in the font.
+ */
+uint16_t dt_get_font_height(dt_font* font);
+
+/*
+ * Allocates a new context and stores the result in ctx
+ */
+dt_context* dt_create_context(xcb_connection_t *dpy, xcb_window_t win);
+/**
+ * Frees an allocated context
+ */
+void dt_free_context(dt_context *ctx);
+
+dt_font* dt_load_font(xcb_connection_t *dis, char const *name, int size);
+void dt_free_font(xcb_connection_t *dis, dt_font *fnt);
+
+/**
+ * draws the string composed of the first `len` characters of `txt`,
+ * drawn with font `fnt`, in color `color`, with the baseline starting at position
+ * x, y.
+ */
+int dt_draw(dt_context *ctx, dt_font *fnt, uint32_t color,
+                 uint32_t x, uint32_t y, char const *txt, size_t len);
+
+/**
+ * Returns the width in pixels that the specified text will take when drawn
+ */
+uint16_t dt_get_text_width(xcb_connection_t* dis, dt_font *fnt, char const *txt, size_t len) ;
+
+/**
+ * Splits txt into N lines by by total length
+ *
+ * Breaks up text into N lines such that strings are generally less than width pixels
+ * @return N, the new number of strings
+ */
+int dt_word_wrap_n(xcb_connection_t* dis, dt_font *fnt, char *txt, int num_lines, uint32_t width);
+
+/**
+ * Replace all instances of '\n' with \0.
+ *
+ * Split txt into N seperate string by replacing the new line marker
+ * with the null string.
+ * @return N, the new number of strings
+ */
+int dt_split_lines(char *txt);
+
+/**
+ * Splits txt into N lines by '\n' and by total length
+ *
+ * Breaks up text into N lines such that each human line is its own string
+ * and string are generally less than width pixels
+ * @return N, the new number of strings
+ */
+int dt_word_wrap_line(xcb_connection_t*dis, dt_font *fnt, char *txt, uint32_t width);
+
+/**
+ * Calls dt_draw for each line such that each line is drawn right after each other
+ */
+int dt_draw_all_lines(dt_context *ctx, dt_font *fnt, uint32_t color,
+        uint32_t x, uint32_t starting_y, uint32_t padding, char const *lines, int num_lines);
+
+#ifdef DTEXT_XCB_IMPLEMENTATION
+
+#include <string.h>
 #include <freetype2/ft2build.h>
-#include <xcb/render.h>
 #include <xcb/render.h>
 #include <xcb/xcb.h>
 #include <xcb/xcb_renderutil.h>
 #include FT_ADVANCES_H
 #include FT_FREETYPE_H
 
-#include "dtext.h"
-
-#define max(a, b) ((a) > (b) ? (a) : (b))
-#define min(a, b) ((a) < (b) ? (a) : (b))
+#define DTEXT_XCB_MAX(a, b) ((a) > (b) ? (a) : (b))
+#define DTEXT_XCB_MIN(a, b) ((a) < (b) ? (a) : (b))
 
 struct dt_context {
     xcb_connection_t* dis;
@@ -80,7 +161,7 @@ static dt_pair const * hash_get(dt_row map[DT_HASH_SIZE], uint8_t key) {
     return NULL;
 }
 
-static int hash_set(dt_row map[DT_HASH_SIZE], dt_pair val) {
+static int dt_hash_set(dt_row map[DT_HASH_SIZE], dt_pair val) {
     dt_row row;
     dt_pair *d;
     size_t i;
@@ -142,7 +223,7 @@ static int load_char(xcb_connection_t* dis, dt_font *fnt, char c) {
 
     g.width  = slot->bitmap.width;
     g.height = slot->bitmap.rows;
-    g.x = - slot->bitmap_left;
+    g.x = -slot->bitmap_left;
     g.y =   slot->bitmap_top;
     g.x_off = slot->advance.x >> 6;
     g.y_off = slot->advance.y >> 6;
@@ -160,7 +241,7 @@ static int load_char(xcb_connection_t* dis, dt_font *fnt, char c) {
 
     free(img);
 
-    return hash_set(fnt->advance, (dt_pair) {
+    return dt_hash_set(fnt->advance, (dt_pair) {
         .c = c,
         .adv = slot->advance.x >> 6,
         .asc = - slot->metrics.horiBearingY >> 6,
@@ -168,7 +249,7 @@ static int load_char(xcb_connection_t* dis, dt_font *fnt, char c) {
     });
 }
 
-static int load_face(FT_Library* ft_lib, FT_Face *face, char const *file, int size) {
+static int dt_load_face(FT_Library* ft_lib, FT_Face *face, char const *file, int size) {
     int err = FT_New_Face(*ft_lib, file, 0, face);
     if (err)
         return err;
@@ -242,7 +323,7 @@ dt_font* dt_load_font(xcb_connection_t *dis, char const *name, int size) {
     }
 
     for (i = 0; i < fnt->num_faces; ++i) {
-        if (load_face(&fnt->ft_lib, &fnt->faces[i], name, size)) {
+        if (dt_load_face(&fnt->ft_lib, &fnt->faces[i], name, size)) {
             while (--i != (size_t) -1)
                 FT_Done_Face(fnt->faces[i]);
             free(fnt->faces);
@@ -258,8 +339,8 @@ dt_font* dt_load_font(xcb_connection_t *dis, char const *name, int size) {
 
     fnt->ascent = descent = 0;
     for (i = 0; i < fnt->num_faces; ++i) {
-        fnt->ascent = max(fnt->ascent, fnt->faces[i]->ascender >> 6);
-        descent = min(descent, fnt->faces[i]->descender >> 6);
+        fnt->ascent = DTEXT_XCB_MAX(fnt->ascent, fnt->faces[i]->ascender >> 6);
+        descent = DTEXT_XCB_MIN(descent, fnt->faces[i]->descender >> 6);
     }
     fnt->height = fnt->ascent - descent;
 
@@ -315,3 +396,65 @@ int dt_draw(dt_context *ctx, dt_font *fnt, uint32_t color,
 
     return 0;
 }
+
+
+int split_lines(char *txt) {
+    char* ptr = txt;
+    int num_lines;
+    for (num_lines = 0; ptr; num_lines++) {
+        ptr = strstr(ptr, "\n");
+        if (ptr) {
+            *ptr = 0;
+            ptr++;
+        }
+    }
+    return num_lines;
+}
+
+int word_wrap_n(xcb_connection_t*dis , dt_font *fnt, char *txt, int num_lines, uint32_t width) {
+    char*ptr = txt;
+    for (int i = 0; i < num_lines; i++) {
+        if (dt_get_text_width(dis, fnt, ptr, strlen(ptr)) > width) {
+            char* whitespace = ptr;
+            char* space = NULL;
+            while (whitespace) {
+                space = strstr(whitespace + 1, " ");
+                if (!space)
+                    break;
+                int sublen = space - ptr;
+                if (dt_get_text_width(dis, fnt, ptr, sublen) > width)
+                    break;
+                whitespace = space;
+            }
+            if (whitespace != ptr) {
+                *whitespace = 0;
+                num_lines++;
+            }
+            else if (space) {
+                *space = 0;
+                num_lines++;
+            }
+        }
+        ptr = ptr + strlen(ptr) + 1;
+    }
+    return num_lines;
+}
+
+int dt_word_wrap_line(xcb_connection_t*dis, dt_font *fnt, char *txt, uint32_t width) {
+    return word_wrap_n(dis, fnt, txt, split_lines(txt), width);
+}
+
+int dt_draw_all_lines(dt_context *ctx, dt_font *fnt, uint32_t color,
+        uint32_t x, uint32_t starting_y, uint32_t padding, char const *lines, int num_lines) {
+
+    int cell_height = padding + dt_get_font_ascent(fnt);
+    const char*ptr = lines;
+    for (int i = 0; i < num_lines; i++) {
+        dt_draw(ctx, fnt, color, x, starting_y + cell_height * (i + 1), ptr, strlen(ptr));
+        ptr = ptr + strlen(ptr) + 1;
+    }
+    return starting_y + cell_height * num_lines;
+}
+
+#endif
+#endif
